d48875862c96d9aa7aa928cbb16e3ca419fa8855
Stefan Hiob <stefan.hiob@outlook.de>
2023-06-16 21:57:51 +0200

    add:vue-tasks

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1>haaaaaa</h1>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
@@ -15,12 +16,13 @@ export default {
 </script>
 
 <style>
-#app {
+@import url(./assets/reset.css);
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
